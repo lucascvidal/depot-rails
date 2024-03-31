9be12443828afe368597b1ef700fec3d168f94a1
Lucas Vidal <lcv55@hotmail.com>
2024-03-31 16:08:08 -0300

    Add cart scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_31_111936) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_31_184811) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
