3f2f1d9dfb65b55fbabe39ec9a199f9af3fe3521
Lucas Vidal <lcv55@hotmail.com>
2024-03-31 18:09:08 -0300

    Combine items in cart via migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_31_201713) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_31_201958) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
