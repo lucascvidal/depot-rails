95f38b6f148c24c60930c6898d04a13093cc3542
Lucas Vidal <lcv55@hotmail.com>
2024-04-05 20:20:01 -0300

    Update system test configuration

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,20 @@
 require "test_helper"
+require "selenium-webdriver"
+require "capybara"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  def setup
+    Capybara.server_host = "0.0.0.0" # bind to all interfaces
+    Capybara.app_host = "http://#{IPSocket.getaddress(Socket.gethostname)}" if ENV["SELENIUM_REMOTE_URL"].present?
+    super
+  end
+
+  url = ENV.fetch("SELENIUM_REMOTE_URL", nil)
+  options = if url
+    { browser: :remote, url: url }
+  else
+    { browser: :chrome }
+  end
+  
+  driven_by :selenium, using: :headless_chrome, options: options
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,32 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
+
+module AuthenticationModule
+  def login_as(user)
+    if respond_to? :visit
+      visit login_url
+      fill_in :name, with: user.name
+      fill_in :password, with: 'secret'
+      click_on 'Login'
+    else
+      post login_url, params: { name: user.name, password: 'secret' }
+    end
+  end
+  
+  def logout
+    delete logout_url
+  end
+  
+  def setup
+    login_as users(:one)
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  include AuthenticationModule
+end
+
+class ActionDispatch::SystemTestCase
+  include AuthenticationModule
+end
