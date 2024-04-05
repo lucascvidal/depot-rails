e31ee55060ee5fa66abf7432cc63c72ee230596f
Lucas Vidal <lcv55@hotmail.com>
2024-04-05 08:33:14 -0300

    Add authorize users method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   include CurrentCart
+  skip_before_action :authorize
   before_action :set_cart, only: %i[ new create ]
   before_action :ensure_cart_is_not_empty, only: %i[ new ]
   before_action :set_order, only: %i[ show edit update destroy ]
