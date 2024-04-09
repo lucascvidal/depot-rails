3e865a7534a352241c56a46819632719f8748d38
Lucas Vidal <lcv55@hotmail.com>
2024-04-09 19:53:16 -0300

    Add optional locale param to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
     delete 'logout' => :destroy
   end
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
   resources :products
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
